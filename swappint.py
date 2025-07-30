48a99f93cbaa6d0af4848e4304f2afdc9fddfa1a
Esha Dilip Wagh <eshawagh094@gmail.com>
2025-07-30 23:42:06 +0530

    Add files via upload

---
+++ swappint.py
@@ -0,0 +1,17 @@
+x=int(input("enter the num x:"))
+y=int(input("enter the num y:"))
+z=x
+x=y
+y=z
+print(f'X{x},Y{y}.')
+
+print(f'after swapping X:{x},Y:{y}.')
+
+
+x=int(input("enter the num x:"))
+y=int(input("enter the num y:"))
+x=x+y
+y=x-y
+x=x-y
+print(f'after swapping X:{x},Y:{y}.')
+
